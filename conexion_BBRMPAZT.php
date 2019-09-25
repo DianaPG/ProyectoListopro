9f20a6dab27605befdb7be1c02f5e8b89ee92927
Diana Perez <55296135+DianaPG@users.noreply.github.com>
2019-09-25 13:06:39 -0500

    Delete conexion_BBRMPAZT.php

--- conexion_BBRMPAZT.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbstr ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =10.213.8.43)(PORT = 1521)) (CONNECT_DATA = (SERVICE_NAME = BBRMPAZT)))";  
-$conn = oci_pconnect('GPAGOS', 'GP4g05_2016',$dbstr) ;
-if (!$conn)
-    {
-       $e = oci_error();
-       trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-    }
-?>
