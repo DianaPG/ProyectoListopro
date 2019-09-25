e480724c1a784bfcee08081d7cfc1401bbdba960
Diana Perez <55296135+DianaPG@users.noreply.github.com>
2019-09-25 13:05:07 -0500

    Delete conexion_FFM.php

--- conexion_FFM.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbstr ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =10.216.16.84)(PORT = 1521)) (CONNECT_DATA = (SERVICE_NAME = GIMTP)))";  
-$conn = oci_pconnect('VIDEOCUBOS', '00',$dbstr) ;
-if (!$conn)
-     {
-       $e = oci_error();
-       trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-     }
-?>
